6881ee2e56d054b0d6f7c533bc0bb54fef48dd54
MOUHALYA <150126005+MOUHALYA@users.noreply.github.com>
2025-07-11 07:12:37 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+#1. How do you check the data type of a variable in Python? Provide an example.
+#int datatype
+a=10
+print(type(a))
+b=8.8
+print(type(b))
+
+
+#2. What is the difference between mutable and immutable data types in Python? Give an example of each.
+#list.mutable
+l=[10,20,30,40,50]
+l[0]
+print(l[0])
